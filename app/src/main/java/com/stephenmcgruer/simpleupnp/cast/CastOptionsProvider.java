d3a5fa3f99cc55187dce86ab49eb4327ad0e65ef
Stephen McGruer <stephen.mcgruer@gmail.com>
2017-03-25 20:08:09 -0400

    Add proper notification/lock screen support and expanded controller
    Clicking on the notification icon will now take you to the app, and clicking on
    the 'now playing' bar will show the default Cast expanded controller.

--- app/src/main/java/com/stephenmcgruer/simpleupnp/cast/CastOptionsProvider.java
+++ app/src/main/java/com/stephenmcgruer/simpleupnp/cast/CastOptionsProvider.java
@@ -20,15 +20,35 @@ import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
+import com.stephenmcgruer.simpleupnp.MainActivity;
 import com.stephenmcgruer.simpleupnp.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class CastOptionsProvider implements OptionsProvider {
     @Override
     public CastOptions getCastOptions(Context context) {
+        List<String> buttonActions = new ArrayList<>();
+        buttonActions.add(MediaIntentReceiver.ACTION_SKIP_PREV);
+        buttonActions.add(MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK);
+        buttonActions.add(MediaIntentReceiver.ACTION_SKIP_NEXT);
+        buttonActions.add(MediaIntentReceiver.ACTION_STOP_CASTING);
+        // Showing "play/pause" and "stop casting" in the compat view of the notification.
+        int[] compatButtonActionsIndicies = new int[]{ 1, 3 };
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setActions(buttonActions, compatButtonActionsIndicies)
+                .setTargetActivityClassName(MainActivity.class.getName())
+                .build();
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .setExpandedControllerActivityClassName(ExpandedControlsActivity.class.getName())
+                .build();
         return new CastOptions.Builder()
+                .setCastMediaOptions(mediaOptions)
                 .setReceiverApplicationId(context.getString(R.string.cast_app_id))
                 .build();
     }
